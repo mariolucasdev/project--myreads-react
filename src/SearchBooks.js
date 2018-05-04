1ec6a89258fc5d5b248129c1cc55f0c6a2a3a4b3
Mário Lucas <maryosk8bkt@gmail.com>
2018-05-04 08:52:22 -0300

    add component SearchBooks

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import ListBooks from './ListBooks';
 
-export default props => (
-  <div className="search-books">
-    <div className="search-books-bar">
-      <Link to="/" className="close-search">
-        Close
-      </Link>
-      <div className="search-books-input-wrapper">
-        {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+export default class SearchBooks extends Component {
+  state = {
+    books: [],
+    query: ''
+  };
 
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-        <input type="text" placeholder="Search by title or author" />
+  handleChange = value => {
+    BooksAPI.search(value).then(res => this.setState({ books: res }));
+  };
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.search}
+              onChange={e => this.handleChange(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            <ListBooks books={this.state.books} />
+          </ol>
+        </div>
       </div>
-    </div>
-    <div className="search-books-results">
-      <ol className="books-grid" />
-    </div>
-  </div>
-);
+    );
+  }
+}
